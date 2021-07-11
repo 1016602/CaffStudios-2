bb4639105aa3c4e59c6a8f0d834ab771aab7cf67
keith1755 <1016602@student.sae.edu.au>
2021-07-11 16:14:01 +1000

    taser script update
    +sound effect
    +FX

--- Caff Studios Project/Assets/Scripts/_TaserGun.cs
+++ Caff Studios Project/Assets/Scripts/_TaserGun.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class _TaserGun : MonoBehaviour
 {
     public Camera fpsCam;
+    public float range = 10f;
+    public AudioSource taserShot;
+    public float fireRate = 15f;
 
+    private float nextTimeToFire = 0f;
+
+    public ParticleSystem electFlash;
+    public GameObject impactEffect;
 
 
     void Start()
@@ -15,21 +22,27 @@ public class _TaserGun : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             FireTaser();
         }
     }
 
     void FireTaser()
     {
+        electFlash.Play();
+        taserShot.Play();
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 20f))
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             if (hit.collider.gameObject.name == "Training model (1)")
             {
                 hit.collider.gameObject.GetComponent<k_EnemyStatsUI>().stuning = true;
+                GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGo, 4f);
             }
         }
+
     }
 }
