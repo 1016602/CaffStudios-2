e1491a03e7f7560f71e8b65ee9084dcbe63d8ac5
ReeceBeckman <reecebeckmansawyer@gmail.com>
2021-08-08 12:55:29 +1000

    Revert "Revert "Merge branch 'main' of https://github.com/1016602/CaffStudios-2 into main""
    This reverts commit 1490fde0af40da2e108b29414107015032fcf268.

--- Caff Studios Project/Assets/Scripts/Player/Gun.cs
+++ Caff Studios Project/Assets/Scripts/Player/Gun.cs
@@ -19,6 +19,7 @@ public class Gun : MonoBehaviour
 
 
     private float nextTimeToFire = 0f;
+    public bool training = false;
 
     void Start()
     {
@@ -66,6 +67,12 @@ public class Gun : MonoBehaviour
 
            GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
            Destroy(impactGo, 2f);
+
+
+            if (hit.collider.gameObject.tag == "Training")
+            {
+                training = true;
+            }
         }
     }
 }
