3ba429edef7e9d08955e38331d58c06544e694b8
keith1755 <1016602@student.sae.edu.au>
2021-08-14 13:56:01 +1000

    Main Menu UI update

--- Caff Studios Project/Assets/Scenes/Tutorial Mission/_TutorMessagePop.cs
+++ Caff Studios Project/Assets/Scenes/Tutorial Mission/_TutorMessagePop.cs
@@ -7,12 +7,21 @@ using MissionCheckList;
 public class _TutorMessagePop : MonoBehaviour
 {
     public GameObject popUpUI;
+    public Animator anim;
+
+    void Start()
+    {
+        anim = anim.GetComponent<Animator>();
+        
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             popUpUI.SetActive(true);
+
+            anim.SetBool("up", true);
         }
     }
 
@@ -20,7 +29,17 @@ public class _TutorMessagePop : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            popUpUI.SetActive(false);
+            
+
+            StartCoroutine(HideMessage());
         }
+    }
+
+    IEnumerator HideMessage()
+    {
+        anim.SetBool("down", true);
+        yield return new WaitForSeconds(1.5f);
+
+        popUpUI.SetActive(false);
     }
 }
